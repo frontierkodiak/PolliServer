c071aa3cacc6f37d9c5c99f05d5cee4175b1df64
frontierkodiak <frontierkodiak@gmail.com>
2023-09-21 16:17:05 +0000

    Gracefully handle swarm_status for no active pods

--- PolliServer/helpers/grabbers.py
+++ PolliServer/helpers/grabbers.py
@@ -12,7 +12,6 @@ from PolliServer.logger.logger import LoggerSingleton
 logger = LoggerSingleton().get_logger()
 
 
-
 async def grab_swarm_status(db: AsyncSession):
     '''
     Get swarm status from PodRecord MySQL database records.
@@ -40,13 +39,43 @@ async def grab_swarm_status(db: AsyncSession):
         stmt = select(PodRecord).filter(PodRecord.last_seen > cutoff_time)
         result = await db.execute(stmt)
         records = result.scalars().all()
-        
+
+        if not records:  # If there are no records, return a list with a single status object with all fields as None
+            return [{
+                'podID': None,
+                'connection_status': None,
+                'rssi': None,
+                'stream_type': None,
+                'loc_name': None,
+                'loc_lat': None,
+                'loc_lon': None,
+                'queue_length': None,
+                'total_frames': None,
+                'last_S1_class': None,
+                'last_S2_class': None,
+                'total_specimens': None,
+                'last_specimen_created_time': None,
+                'last_seen': None,
+                'time_since_last_seen': None,
+                'time_since_last_specimen': None
+            }]
+
         swarm_status = []  # Initialize as a list
         for record in records:
+            
+            if record.last_seen:
+                time_since_last_seen = datetime.datetime.utcnow() - record.last_seen
+            else:
+                time_since_last_seen = 0
+            if record.last_specimen_created_time:
+                time_since_last_specimen = datetime.datetime.utcnow() - record.last_specimen_created_time
+            else:
+                time_since_last_specimen = 0
 
             pod_status = {
                 'podID': record.name,
                 'connection_status': record.connection_status,
+                'rssi': record.rssi,
                 'stream_type': record.stream_type,
                 'loc_name': record.location_name,
                 'loc_lat': record.latitude,
@@ -57,7 +86,9 @@ async def grab_swarm_status(db: AsyncSession):
                 'last_S2_class': record.last_S2_class,
                 'total_specimens': record.total_specimens,
                 'last_specimen_created_time': record.last_specimen_created_time.strftime(DATETIME_FORMAT_STRING) if record.last_specimen_created_time else None,
-                'last_seen': record.last_seen.strftime(DATETIME_FORMAT_STRING) if record.last_seen else None
+                'last_seen': record.last_seen.strftime(DATETIME_FORMAT_STRING) if record.last_seen else None,
+                'time_since_last_seen': time_since_last_seen.total_seconds() / 60.0 if record.last_seen else None,
+                'time_since_last_specimen': time_since_last_specimen.total_seconds() / 60.0 if record.last_specimen_created_time else None
             }
             swarm_status.append(pod_status)  # Append each status object to the list
 
@@ -67,7 +98,6 @@ async def grab_swarm_status(db: AsyncSession):
         traceback.print_exc()  # This will print the traceback to the console.
         # Re-raise the exception so that the calling function can catch it
         raise e
-
 
 
 def build_timeline_data_query(db_session, start_date=None, end_date=None, podID=None, location=None, 
